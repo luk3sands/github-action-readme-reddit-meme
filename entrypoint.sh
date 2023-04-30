3cf5813bd1ded7f9ad0423470202d15fad290db1
Luke Sands <luke.sands@d3code.com.au>
2023-04-30 18:34:14 +1000

    Updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/sh -l
-COMMIT_EMAIL="$(git log -1 --pretty=format:'%ae')"
-COMMIT_NAME="$(git log -1 --pretty=format:'%ab')"
-
-git config --global user.email "$GIT_EMAIL"
-git config --global user.name "$GIT_NAME"
 
 commit() {
+  echo "\Updated" >> README.md
   git add README.md
   git commit -m "Update README.md"
   git push origin master
 }
-
-echo "\Updated" >> README.md
 
 commit
 
