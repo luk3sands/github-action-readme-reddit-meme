45860c54736b2caaa8ba7008b6410d19f0b409ad
Luke Sands <luke.sands@d3code.com.au>
2023-04-30 18:17:52 +1000

    Updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,11 @@
 GIT_NAME="GitHub Actions"
 GIT_EMAIL="github-actions[bot]@users.noreply.github.com"
 
-check_commit() {
-  local COMMIT_EMAIL="$(git log -1 --pretty=format:'%ae')"
-  echo $COMMIT_EMAIL
-  if [ $COMMIT_EMAIL == "$GIT_EMAIL" ]
-  then
-  echo "This is a bot commit"
-  exit 1
-  fi
-}
-
 commit() {
-  git config --global user.name "$GIT_NAME"
-  git config --global user.email "$GIT_EMAIL"
-  git add .
+  git add README.md
   git commit -m "Update README.md"
   git push origin master
 }
-
-check_commit
 
 echo "\nUpdated README.md" >> README.md
 
