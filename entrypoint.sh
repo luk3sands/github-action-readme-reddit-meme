5da22373348f6f7d68e9ec258e33b6dc9326a8b1
GitHub <github-actions[bot]@users.noreply.github.com>
2023-04-30 17:56:39 +1000

    Updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,15 @@
-#!/bin/sh -l
+#!/bin/bash
 
 GIT_NAME="GitHub Actions"
 GIT_EMAIL="github-actions[bot]@users.noreply.github.com"
 
-function check_commit() {
+check_commit() {
   local COMMIT_EMAIL="$(git log -1 --pretty=format:'%ae')"
   echo $COMMIT_EMAIL
   
-  if [ $COMMIT_EMAIL == $GIT_EMAIL ]
+  if [ $COMMIT_EMAIL == $GIT_EMAIL ];
   then
       echo "This is a bot commit"
       exit 1
   fi
 }
-
-check_commit
-
-git config --global user.name $GIT_NAME
-git config --global user.email $GIT_EMAIL
-
-git status -s
