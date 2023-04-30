3a86694a3763f1a4c4c690ea9e511b9cb8a52fee
GitHub <github-actions[bot]@users.noreply.github.com>
2023-04-30 18:00:15 +1000

    Update README.md

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,39 @@
-#!/bin/bash
+#!/bin/sh -l
+
+#!/bin/sh -l
+
+# git status
+
+
 
 GIT_NAME="GitHub Actions"
 GIT_EMAIL="github-actions[bot]@users.noreply.github.com"
 
-check_commit() {
+function check_commit() {
   local COMMIT_EMAIL="$(git log -1 --pretty=format:'%ae')"
   echo $COMMIT_EMAIL
   
-  if [ $COMMIT_EMAIL == $GIT_EMAIL ];
+  if [ $COMMIT_EMAIL == "GIT_EMAIL" ]
   then
       echo "This is a bot commit"
       exit 1
   fi
 }
+
+function commit() {
+
+  git config --global user.name $GIT_NAME
+  git config --global user.email $GIT_EMAIL
+
+  git add .
+  git commit -m "Update README.md"
+  git push origin master
+}
+
+check_commit
+commit
+
+git status -s
+
+git config --global user.name "Luke Sands"
+git config --global user.email "luke.sands@d3code.com.au"
